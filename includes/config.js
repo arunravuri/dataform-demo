2333292f193cbb2413381848013d661252563f21
Arun Kumar <ravuriarun.freelance@gmail.com>
2024-04-04 13:29:48 +0000

    Adding translation DB tables

--- includes/config.js
+++ includes/config.js
@@ -1,6 +1,9 @@
 const env = {
-    declarationSchema: dataform.projectConfig.defaultSchema + '_' + dataform.projectConfig.schemaSuffix
+    declarationSchema: dataform.projectConfig.defaultSchema + '_' + dataform.projectConfig.schemaSuffix,
+    translationDeclarationSchema: dataform.projectConfig.vars.translationDataset + '_' + dataform.projectConfig.schemaSuffix
 }    
 
 
-module.exports = {env};
+module.exports = {
+    env
+};
