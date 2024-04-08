0b776cc2c040c3e8481fe0b0d4ebedfcdc3c9fe1
Arun Kumar <ravuriarun.freelance@gmail.com>
2024-04-08 08:50:39 +0000

    TH payment report updates

--- includes/config.js
+++ includes/config.js
@@ -1,6 +1,8 @@
 const env = {
     declarationSchema: dataform.projectConfig.defaultSchema + '_' + dataform.projectConfig.schemaSuffix,
-    translationDeclarationSchema: dataform.projectConfig.vars.translationDataset + '_' + dataform.projectConfig.schemaSuffix
+    translationDeclarationSchema: dataform.projectConfig.vars.translationDataset + '_' + dataform.projectConfig.schemaSuffix,
+    thPaymentProjects: dataform.projectConfig.vars.thPaymentProjects
+    
 }    
 
 
